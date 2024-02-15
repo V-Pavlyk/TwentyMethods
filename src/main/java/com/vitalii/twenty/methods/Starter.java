e52103df5543dad02d922aeb5adb8c1d735b68bf
Vitalii Pavlyk <pavlick.v@gmail.com>
2024-02-15 13:10:09 +0200

    refactor Method 1-2

--- src/main/java/com/vitalii/twenty/methods/Starter.java
+++ src/main/java/com/vitalii/twenty/methods/Starter.java
@@ -2,6 +2,9 @@ package com.vitalii.twenty.methods;
 
 
 public class Starter {
+
+    public static final String STRING = "******";
+
     public static void main(String[] args) {
 
         System.out.println("Testing my methods");
@@ -9,17 +12,17 @@ public class Starter {
         //1 принимает масив чаров, выводит его на экран
         System.out.println("1. printCharArrayToConsole");
         TwentyMethods.printCharArrayToConsole(new char[]{'H', 'e', '1', '1', 'o'});
-        System.out.println("******");
+        System.out.println(STRING);
         TwentyMethods.printCharArrayToConsole(new char[]{});
-        System.out.println("******");
+        System.out.println(STRING);
 
         //2 принимает массив интов, возвращает массив чаров,
         //каждый символ в позиции массива соответствует коду символа передаваемого инта
         System.out.println("2 changeCharArrayToIntArray");
         System.out.println(TwentyMethods.modifyCharArrayToIntArray(new int[]{4, 6}));
-        System.out.println("******");
+        System.out.println(STRING);
         System.out.println(TwentyMethods.modifyCharArrayToIntArray(new int[]{}));
-        System.out.println("******");
+        System.out.println(STRING);
 
 
     }

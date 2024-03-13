8dd07b8d43905d51ef5812ebcb5b7fa4e480734a
Vitalii Pavlyk <pavlick.v@gmail.com>
2024-03-13 18:03:17 +0200

    JEP-56-Methods-6-12

--- src/main/java/com/vitalii/twenty/methods/Starter.java
+++ src/main/java/com/vitalii/twenty/methods/Starter.java
@@ -6,39 +6,121 @@ import static com.vitalii.twenty.methods.TwentyMethods.*;
 public class Starter {
 
     public static final String STRING = "******";
+    public static final String STRING2 = "******\n";
 
     public static void main(String[] args) {
 
         System.out.println("Testing my methods");
 
-        //1 принимает масив чаров, выводит его на экран
+        /**
+         * 1 принимает масив чаров, выводит его на экран
+         */
+        System.out.println(STRING);
         System.out.println("1. printCharArrayToConsole");
         TwentyMethods.printCharArrayToConsole(new char[]{'H', 'e', '1', '1', 'o'});
         System.out.println(STRING);
         TwentyMethods.printCharArrayToConsole(new char[]{});
         System.out.println(STRING);
 
-        //2 принимает массив интов, возвращает массив чаров,
-        //каждый символ в позиции массива соответствует коду символа передаваемого инта
-        System.out.println("2 changeCharArrayToIntArray");
+        /**
+         * 2 принимает массив интов, возвращает массив чаров,
+         * каждый символ в позиции массива соответствует коду символа передаваемого инта
+         */
+        System.out.println("2. changeCharArrayToIntArray");
         System.out.println(TwentyMethods.modifyCharArrayToIntArray(new int[]{4, 6}));
         System.out.println(STRING);
         System.out.println(TwentyMethods.modifyCharArrayToIntArray(new int[]{}));
         System.out.println(STRING);
 
-        //3 приминает 2 инта, а и б, возвращает большее из этих 2х чисел.
+        /**
+         * 3 принимает 2 инта, а и б, возвращает большее из этих 2х чисел.
+         */
+        System.out.println("3. returnMaxOfTwoInts");
         System.out.println(returnMaxOfTwoInts(100500, 300));
+        System.out.println(STRING2);
 
-        //3.1 приминает 2 инта, а и б, возвращает большее из этих 2х чисел.
+
+        /**
+         * 3.1 принимает 2 инта, а и б, возвращает большее из этих 2х чисел.
+         */
+        System.out.println(STRING);
+        System.out.println("3.1. returnMaxOfTwoIntsWithMath");
         System.out.println(returnMaxOfTwoIntsWithMath(32, 23));
+        System.out.println(STRING2);
 
-        //3.2 приминает 2 инта, а и б, возвращает большее из этих 2х чисел.
+        /**
+         * 3.2 принимает 2 инта, а и б, возвращает большее из этих 2х чисел.
+         */
+        System.out.println(STRING);
+        System.out.println("3.2. returnMaxOfTwoIntsWithThernary");
         System.out.println(returnMaxOfTwoIntsWithThernary(20, 10));
+        System.out.println(STRING2);
 
-        //4 принимает 3 инта, возвращает большее из них
+        /**
+         * 4 принимает 3 инта, возвращает большее из них
+         */
+        System.out.println(STRING);
+        System.out.println("4. returnMaxOfThreeInts");
         System.out.println(returnMaxOfThreeInts(5, 88, 10));
+        System.out.println(STRING2);
 
-        //5 приминает 5 интов, возвращает большее из них
+        /**
+         * 5 принимает 5 интов, возвращает большее из них
+         */
+        System.out.println(STRING);
+        System.out.println("5. returnMaxOfFiveIntsWithVarargs");
         System.out.println(returnMaxOfFiveIntsWithVarargs(2, 3, 125, 5, 99));
+        System.out.println(STRING2);
+
+        /**
+         * 6 принимает массив чаров, возвращает строку состоящую из символов массива
+         */
+        System.out.println(STRING);
+        System.out.println("6. return StringOfArrayChars");
+        System.out.println(returnStringOfArrayChars(new char[]{'H', 'e', 'l', 'l', 'o', '!'}));
+        System.out.println(STRING2);
+
+        /**
+         * 8 принимает массив интов, и значение типа инт, возвращает индекс массива в котором
+         * значение совпадает с передаваемым, начиная с начала массива. Если значения в массиве нет возвращает -1
+         */
+        System.out.println(STRING);
+        System.out.println("8. returns the index from the beginning of array");
+        System.out.println(returnsIndexFromBeginningOfArray(new int[]{8, 10, 33, 5, 25, 7, 100}, 33));
+        System.out.println(STRING2);
+
+
+        /**
+         * 9 принимает массив интов, и значение типа инт, возвращает индекс массива в котором
+         * значение совпадает с передаваемым, начиная с конца массива. Если значения в массиве нет возвращает -1
+         */
+        System.out.println(STRING);
+        System.out.println("9. returnsTheIndexFromTheEndOfArray");
+        System.out.println(returnsIndexFromEndOfArray(new int[]{1, 99, 150, 6, 41, 11, 29, 53, 32, 1000}, 11));
+        System.out.println(STRING2);
+
+        /**
+         * 10 метод принимает инт, и возвращает факториал от заданого инта
+         */
+        System.out.println(STRING);
+        System.out.println("10. returnFactorialOfTheGivenInt");
+        System.out.println(returnFactorial(8));
+        System.out.println(STRING2);
+
+        /**
+         * 11 принимает инт год, и возвращает тру если год высокосный
+         */
+        System.out.println(STRING);
+        System.out.println("IsThisALeapYear");
+        System.out.println(IsThisLeapYear(1900));
+        System.out.println(STRING2);
+
+        /**
+         * 12 принимает массив интов и число, выводит на екран только елементы массива которые кратны этому числу
+         */
+        System.out.println(STRING);
+        System.out.println("displaysMultipleElementsOfArray");
+        TwentyMethods.displaysMultipleElementsOfArray(new int[]{1, 10, 5, 2, 100, 60, 33, 9, 12}, 3);
+        System.out.println(STRING2);
     }
 }
--- src/main/java/com/vitalii/twenty/methods/TwentyMethods.java
+++ src/main/java/com/vitalii/twenty/methods/TwentyMethods.java
@@ -2,13 +2,17 @@ package com.vitalii.twenty.methods;
 
 public class TwentyMethods {
 
-    //1 принимает масив чаров, выводит его на экран
+    /**
+     * 1 принимает массив чаров, выводит его на экран
+     */
     public static void printCharArrayToConsole(char[] chars) {
         System.out.println(chars);
     }
 
-    //2 принимает массив интов, возвращает массив чаров,
-    //каждый символ в позиции массива соответствует коду символа передаваемого инта
+    /**
+     * 2 принимает массив интов, возвращает массив чаров,
+     * каждый символ в позиции массива соответствует коду символа передаваемого инта
+     */
     public static char[] modifyCharArrayToIntArray(int[] ints) {
         char[] chars = new char[ints.length];
         for (int i = 0; i < ints.length; i++) {
@@ -17,7 +21,9 @@ public class TwentyMethods {
         return chars;
     }
 
-    //3 приминает 2 инта, а и б, возвращает большее из этих 2х чисел
+    /**
+     * 3 принимает 2 инта, а и б, возвращает большее из этих 2х чисел.
+     */
     public static int returnMaxOfTwoInts(int a, int b) {
         if (a > b) {
             return a;
@@ -25,23 +31,31 @@ public class TwentyMethods {
         return b;
     }
 
-    //3.1 приминает 2 инта, а и б, возвращает большее из этих 2х чисел
+    /**
+     * 3.1 принимает 2 инта, а и б, возвращает большее из этих 2х чисел
+     */
     public static int returnMaxOfTwoIntsWithMath(int c, int d) {
         return Math.max(c, d);
     }
 
-    //3.2 приминает 2 инта, а и б, возвращает большее из этих 2х чисел
+    /**
+     * 3.2 принимает 2 инта, а и б, возвращает большее из этих 2х чисел
+     */
     public static int returnMaxOfTwoIntsWithThernary(int a, int b) {
         return (a > b) ? a : b;
     }
 
-    //4 принимает 3 инта, возвращает большее из них
+    /**
+     * 4 принимает 3 инта, возвращает большее из них
+     */
     public static int returnMaxOfThreeInts(int a, int b, int c) {
         int tempMax = returnMaxOfTwoInts(a, b);
         return returnMaxOfTwoInts(tempMax, c);
     }
 
-    //5 приминает 5 интов, возвращает большее из них
+    /**
+     * 5 принимает 5 интов, возвращает большее из них
+     */
     public static int returnMaxOfFiveIntsWithVarargs(int... nums) {
         int max = nums[0];
         for (int num : nums) {
@@ -51,4 +65,75 @@ public class TwentyMethods {
         }
         return max;
     }
+
+    /**
+     * 6 принимает массив чаров, возвращает строку состоящую из символов массива
+     */
+    public static String returnStringOfArrayChars(char[] array) {
+        return new String(array);
+    }
+
+    /**
+     * 8 принимает массив интов, и значение типа инт, возвращает индекс массива в котором
+     * значение совпадает с передаваемым, начиная с начала массива. Если значения в массиве нет возвращает -1
+     */
+    public static int returnsIndexFromBeginningOfArray(int[] array, int value) {
+        for (int i = 0; i < array.length; i++) {
+            if (value == array[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 9 принимает массив интов, и значение типа инт, возвращает индекс массива в котором
+     * значение совпадает с передаваемым, начиная с конца массива. Если значения в массиве нет возвращает -1
+     */
+    public static int returnsIndexFromEndOfArray(int[] array, int value) {
+        for (int i = array.length - 1; i >= 1; i--) {
+            if (value == array[i]) {
+                return i;
+            }
+
+        }
+        return -1;
+    }
+
+    /**
+     * 10 метод принимает инт, и возвращает факториал от заданого инта
+     */
+    public static int returnFactorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+
+    /**
+     * 11 принимает инт год, и возвращает тру если год высокосный
+     */
+    public static boolean IsThisLeapYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                return year % 400 == 0;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 12 принимает массив интов и число, выводит на екран только елементы массива которые кратны этому числу
+     */
+    public static void displaysMultipleElementsOfArray(int[] array, int num) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % num == 0) {
+                System.out.println(array[i]);
+            }
+        }
+    }
 }
+
+

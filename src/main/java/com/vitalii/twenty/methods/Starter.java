3c4cd239c95bced26bba82a57aec607443755b16
Vitalii Pavlyk <pavlick.v@gmail.com>
2024-03-15 11:08:33 +0200

    JEP-56-Methods-6-12-refactor

--- src/main/java/com/vitalii/twenty/methods/Starter.java
+++ src/main/java/com/vitalii/twenty/methods/Starter.java
@@ -6,7 +6,7 @@ import static com.vitalii.twenty.methods.TwentyMethods.*;
 public class Starter {
 
     public static final String STRING = "******";
-    public static final String STRING2 = "******\n";
+    public static final String STRING_2 = "******\n";
 
     public static void main(String[] args) {
 
@@ -18,26 +18,24 @@ public class Starter {
         System.out.println(STRING);
         System.out.println("1. printCharArrayToConsole");
         TwentyMethods.printCharArrayToConsole(new char[]{'H', 'e', '1', '1', 'o'});
-        System.out.println(STRING);
-        TwentyMethods.printCharArrayToConsole(new char[]{});
-        System.out.println(STRING);
+        System.out.println(STRING_2);
 
         /**
          * 2 принимает массив интов, возвращает массив чаров,
          * каждый символ в позиции массива соответствует коду символа передаваемого инта
          */
+        System.out.println(STRING);
         System.out.println("2. changeCharArrayToIntArray");
-        System.out.println(TwentyMethods.modifyCharArrayToIntArray(new int[]{4, 6}));
-        System.out.println(STRING);
-        System.out.println(TwentyMethods.modifyCharArrayToIntArray(new int[]{}));
-        System.out.println(STRING);
+        System.out.println(TwentyMethods.getCharsFromInts(new int[]{4, 6}));
+        System.out.println(STRING_2);
 
         /**
          * 3 принимает 2 инта, а и б, возвращает большее из этих 2х чисел.
          */
-        System.out.println("3. returnMaxOfTwoInts");
-        System.out.println(returnMaxOfTwoInts(100500, 300));
-        System.out.println(STRING2);
+        System.out.println(STRING);
+        System.out.println("3. getMaxOfTwoInts");
+        System.out.println(getMaxOfTwoInts(100500, 300));
+        System.out.println(STRING_2);
 
 
         /**
@@ -45,16 +43,24 @@ public class Starter {
          */
         System.out.println(STRING);
         System.out.println("3.1. returnMaxOfTwoIntsWithMath");
-        System.out.println(returnMaxOfTwoIntsWithMath(32, 23));
-        System.out.println(STRING2);
+        System.out.println(getMaxOfTwoIntsWithMath(32, 23));
+        System.out.println(STRING_2);
 
         /**
-         * 3.2 принимает 2 инта, а и б, возвращает большее из этих 2х чисел.
+         * 3.2.1 принимает 2 инта, а и б, возвращает большее из этих 2х чисел.
          */
         System.out.println(STRING);
-        System.out.println("3.2. returnMaxOfTwoIntsWithThernary");
-        System.out.println(returnMaxOfTwoIntsWithThernary(20, 10));
-        System.out.println(STRING2);
+        System.out.println("3.2. getMaxOfTwoIntsWithThernary");
+        System.out.println(getMaxOfTwoIntsWithThernary(20, 20));
+        System.out.println(STRING_2);
+
+        /**
+         * 3.2.2 принимает 2 инта, а и б, возвращает большее из этих 2х чисел.
+         */
+        System.out.println(STRING);
+        System.out.println("3.2. getMaxOfTwoIntsWithThernary");
+        System.out.println(getMaxOfTwoIntsWithThernary(20, 10));
+        System.out.println(STRING_2);
 
         /**
          * 4 принимает 3 инта, возвращает большее из них
@@ -62,15 +68,23 @@ public class Starter {
         System.out.println(STRING);
         System.out.println("4. returnMaxOfThreeInts");
         System.out.println(returnMaxOfThreeInts(5, 88, 10));
-        System.out.println(STRING2);
+        System.out.println(STRING_2);
 
         /**
          * 5 принимает 5 интов, возвращает большее из них
          */
         System.out.println(STRING);
         System.out.println("5. returnMaxOfFiveIntsWithVarargs");
-        System.out.println(returnMaxOfFiveIntsWithVarargs(2, 3, 125, 5, 99));
-        System.out.println(STRING2);
+        System.out.println(returnMaxOfFiveIntsWithVarargs(150, 3, 125, 5, 99));
+        System.out.println(STRING_2);
+
+        /**
+         * 5 принимает 5 интов, возвращает большее из них
+         */
+        System.out.println(STRING);
+        System.out.println("5. returnMaxOfFiveIntsWithVarargs");
+        System.out.println(returnMaxOfFiveIntsWithVarargs(150, 3, 125, 5, 99, 1000));
+        System.out.println(STRING_2);
 
         /**
          * 6 принимает массив чаров, возвращает строку состоящую из символов массива
@@ -78,7 +92,7 @@ public class Starter {
         System.out.println(STRING);
         System.out.println("6. return StringOfArrayChars");
         System.out.println(returnStringOfArrayChars(new char[]{'H', 'e', 'l', 'l', 'o', '!'}));
-        System.out.println(STRING2);
+        System.out.println(STRING_2);
 
         /**
          * 8 принимает массив интов, и значение типа инт, возвращает индекс массива в котором
@@ -86,8 +100,8 @@ public class Starter {
          */
         System.out.println(STRING);
         System.out.println("8. returns the index from the beginning of array");
-        System.out.println(returnsIndexFromBeginningOfArray(new int[]{8, 10, 33, 5, 25, 7, 100}, 33));
-        System.out.println(STRING2);
+        System.out.println(getIndexFromBeginningOfArray(new int[]{8, 10, 33, 5, 25, 7, 100}, 33));
+        System.out.println(STRING_2);
 
 
         /**
@@ -96,8 +110,8 @@ public class Starter {
          */
         System.out.println(STRING);
         System.out.println("9. returnsTheIndexFromTheEndOfArray");
-        System.out.println(returnsIndexFromEndOfArray(new int[]{1, 99, 150, 6, 41, 11, 29, 53, 32, 1000}, 11));
-        System.out.println(STRING2);
+        System.out.println(getIndexFromEndOfArray(new int[]{1, 99, 150, 6, 41, 11, 29, 53, 32, 1000}, 11));
+        System.out.println(STRING_2);
 
         /**
          * 10 метод принимает инт, и возвращает факториал от заданого инта
@@ -105,22 +119,22 @@ public class Starter {
         System.out.println(STRING);
         System.out.println("10. returnFactorialOfTheGivenInt");
         System.out.println(returnFactorial(8));
-        System.out.println(STRING2);
+        System.out.println(STRING_2);
 
         /**
          * 11 принимает инт год, и возвращает тру если год высокосный
          */
         System.out.println(STRING);
-        System.out.println("IsThisALeapYear");
-        System.out.println(IsThisLeapYear(1900));
-        System.out.println(STRING2);
+        System.out.println("11. isThisALeapYear");
+        System.out.println(isLeapYear(1900));
+        System.out.println(STRING_2);
 
         /**
          * 12 принимает массив интов и число, выводит на екран только елементы массива которые кратны этому числу
          */
         System.out.println(STRING);
-        System.out.println("displaysMultipleElementsOfArray");
-        TwentyMethods.displaysMultipleElementsOfArray(new int[]{1, 10, 5, 2, 100, 60, 33, 9, 12}, 3);
-        System.out.println(STRING2);
+        System.out.println("12. displayMultipleElementsOfArray");
+        TwentyMethods.displayMultipleElementsOfArray(new int[]{1, 10, 5, 2, 100, 60, 33, 9, 12}, 3);
+        System.out.println(STRING_2);
     }
 }
